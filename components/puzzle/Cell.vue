d2c8a26bc38c335886370a919882e0ca300e8a96
Adam Engebretson <adam.engebretson@getbridge.com>
2022-07-08 12:29:20 -0500

    Adding some more cell shading

--- components/puzzle/Cell.vue
+++ components/puzzle/Cell.vue
@@ -16,9 +16,13 @@ const puzzle = usePuzzle()
 const cellClass = (cell) => ({
   '!text-gray-400': cell.given,
   '!text-red-400': puzzle.answerFor(cell) != cell.value,
-  '!bg-gray-800 hover_!bg-gray-700': cell.x == puzzle.x || cell.y == puzzle.y,
+  '!bg-gray-800 hover_!bg-gray-700': cell.x == puzzle.x || cell.y == puzzle.y || (cell.value && cell.value == puzzle.currentCell.value),
   '!bg-gray-600 hover_!bg-gray-500': cell.x == puzzle.x && cell.y == puzzle.y,
   // '!bg-blue-300': cell.value == puzzle.currentCell.value
+})
+
+const noteClass = (cell, note) => ({
+  '!bg-gray-800 hover_!bg-gray-700': note == puzzle.currentCell.value && cell.notes.includes(note)
 })
 </script>
 
@@ -26,6 +30,6 @@ const cellClass = (cell) => ({
 div(:class="cellClass(cell)" @click="puzzle.select(cell)").aspect-square.flex.justify-center.items-center.text-6xl.text-gray-300.bg-gray-900.hover_bg-gray-800.font-light
   span(v-if="cell.value") {{ cell.value }}
   div(v-else).grid.grid-cols-3.w-full
-    div(v-for="x in 9" :key="x").aspect-square.flex.justify-center.items-center.text-sm
+    div(v-for="x in 9" :key="x" :class="noteClass(cell, x)").aspect-square.flex.justify-center.items-center.text-sm
       span(v-if="cell.notes.includes(x)") {{ x }}
 </template>
